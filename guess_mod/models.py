957886bc801676edf5296b7833f05e6ea4f65423
Mike <m_ran66@hotmail.com>
2015-02-19 22:23:34 -0800

    update Games model date and integer field default values

--- guess_mod/models.py
+++ guess_mod/models.py
@@ -1,33 +1,30 @@
 from django.db import models
-from datetime import datetime
-from datetime import date
-from datetime import time
-from datetime import timedelta
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class Gamer(models.Model):
     user = models.OneToOneField(User)
-    # username = models.CharField(max_length=100)
-    # email = models.EmailField(max_length=254)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __unicode__(self):
         return self.user.username + ", " + self.user.email
+
 
 class Game(models.Model):
     game_date = models.DateField(auto_now_add=True)
     game_start = models.TimeField(null=True)
     game_end = models.TimeField(null=True)
     game_duration = models.TimeField(null=True)
-    total_moves = models.IntegerField()
-    incorrect_moves = models.IntegerField()
+    total_moves = models.IntegerField(default=0)
+    incorrect_moves = models.IntegerField(default=0)
     icon_set = models.CharField(max_length=100)
-    grid_count = models.IntegerField()
+    grid_count = models.IntegerField(default=0)
     player = models.ForeignKey(User)
 
     def __unicode__(self):
-        return self.game_date
+        return self.total_moves
+
 
 class Moves(models.Model):
     game = models.ForeignKey(Game)
